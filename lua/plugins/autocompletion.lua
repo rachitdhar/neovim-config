5f505328e212922bb3495a7e2c2abb44b54e4219
Rachit Dhar <75770595+rachitdhar@users.noreply.github.com>
2025-05-02 10:48:16 +0530

    added plugin for autocompletion

---
+++ lua/plugins/autocompletion.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",     -- buffer completions
+      "hrsh7th/cmp-path",       -- path completions
+      "hrsh7th/cmp-nvim-lsp",   -- LSP completions
+      "L3MON4D3/LuaSnip",       -- snippet engine
+      "saadparwaiz1/cmp_luasnip" -- snippet completions
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  }
+}
